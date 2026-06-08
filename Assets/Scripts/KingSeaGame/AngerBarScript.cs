96515119294587ec2d7c2c89e575e0c1bf48ec62
Jacqueline Christina Boesiger <jacqueline@bosiger.com>
2026-06-08 17:56:06 +0200

    When the boss gets hit by snowballs, it takes damage and the anger meter goes down

---
+++ Assets/Scripts/KingSeaGame/AngerBarScript.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class AngerBarScript : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    public void UpdateAngerBar(float amount)
+    {
+        slider.value = amount;
+    }
+    private void OnEnable()
+    {
+        KingSeaScript.KingSeaTakesDamage += UpdateAngerBar;
+    }
+    
+    private void OnDisable()
+    {
+        KingSeaScript.KingSeaTakesDamage -= UpdateAngerBar;
+    }
+}
---
+++ Assets/Scripts/KingSeaGame/KingSeaScript.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System;
+
+public class KingSeaScript : MonoBehaviour
+{
+    public int kingSeaHealth;
+    [SerializeField] private int kingSeaMaxHealth;
+    [SerializeField] private int snowballDamage;
+    [SerializeField] private int healthPortions;
+    public static event Action <float> KingSeaTakesDamage;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        kingSeaHealth = kingSeaMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void TakingDamage()
+    {
+        kingSeaHealth -= snowballDamage;
+        float amount = (float) kingSeaHealth / (float) kingSeaMaxHealth;
+        KingSeaTakesDamage?.Invoke(amount);
+        CheckHealth();
+    }
+    
+    private void CheckHealth()
+    {
+        if (kingSeaHealth <= 0)
+        {
+            Win();
+        }
+        int threshold = kingSeaHealth - healthPortions;
+        if (kingSeaHealth <= threshold)
+        {
+            threshold -= healthPortions;
+            SwitchSides();
+        }
+    }
+
+    private void SwitchSides()
+    {
+        //TODO The boss will change the position of sword and shield
+    }
+    
+    private void Win()
+    {
+        //TODO what happens when the anger meter goes down
+    }
+
+    // private void OnTriggerEnter(Collider collision)
+    // {
+    //     Debug.Log("trigger hit");
+    //     TakingDamage();
+    //     Destroy(collision.gameObject);
+    // }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        TakingDamage();
+        Destroy(collision.gameObject);
+    }
+}
