e47c0f5c66807745f0a86ff6c0228621e71bfc5e
Lapraniteon <lapraniteon@gmail.com>
2026-06-09 13:50:21 +0200

    Scene moves and darius moves towards player

---
+++ Assets/Scripts/DariusLegsGame/WorldMovement.cs
@@ -0,0 +1,63 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class WorldMovement : MonoBehaviour
+{
+
+    [Header("Movement")]
+    [SerializeField] [Tooltip("Movement speed of the scene in units/second.")] private float globalMovementSpeed;
+    
+    [Header("Floor")]
+    [SerializeField] private Transform floorTransform;
+    private Tween _floorMovementTween;
+
+    [Header("Gates")] 
+    [SerializeField] private Transform gateSpawnPoint;
+    [SerializeField] private GateSet gatePrefab;
+    [SerializeField] [Tooltip("Spawn interval in units.")] private float gateSpawnInterval;
+    private float _despawnDistance = 20f;
+    [SerializeField] [Tooltip("Distance to the player at which the doors close.")] private float doorClosingDistance;
+    private Sequence _gateSpawningLoop;
+    
+    [Header("Darius")]
+    [SerializeField] private DariusController darius;
+    [SerializeField] [Tooltip("Time it takes for the player to have \"caught\" up to Darius.")] private float catchupTime;
+    [SerializeField] private float finalCatchDistance;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartMovement();
+        StartGateSpawning();
+        
+        darius.StartBackwardsMovement(finalCatchDistance, catchupTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartMovement()
+    {
+        _floorMovementTween = floorTransform.DOLocalMoveZ(-100f, 100f / globalMovementSpeed).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+    }
+
+    private void StartGateSpawning()
+    {
+        _gateSpawningLoop = DOTween.Sequence();
+        _gateSpawningLoop
+            .AppendCallback(SpawnGate)
+            .AppendInterval(gateSpawnInterval / globalMovementSpeed)
+            .SetLoops(-1, LoopType.Restart);
+
+        _gateSpawningLoop.Play();
+    }
+
+    private void SpawnGate()
+    {
+        GateSet gate = Instantiate(gatePrefab, gateSpawnPoint.position, Quaternion.identity);
+        gate.StartMovement(_despawnDistance, globalMovementSpeed, doorClosingDistance);
+    }
+}
