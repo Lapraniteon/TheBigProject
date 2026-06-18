2371dcb2b10b164482de4e9bf7ec545a1f6e70a2
Lapraniteon <lapraniteon@gmail.com>
2026-06-18 14:32:57 +0200

    LS Add Players to Target Group on load

--- Assets/Scripts/LightShineGame/LightShineGameManager.cs
+++ Assets/Scripts/LightShineGame/LightShineGameManager.cs
@@ -1,15 +1,32 @@
 using System;
+using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class LightShineGameManager : MonoBehaviour
 {
 
-    public PlayerController[] players;
+    public List<PlayerController> players = new ();
 
     public PatrolController patrolController;
 
+    [SerializeField] private CinemachineTargetGroup cameraTargetGroup;
+
     private void Start()
     {
+        if (GameManager.Instance != null)
+        {
+            foreach (GameObject player in GameManager.Instance.players)
+            {
+                players.Add(player.GetComponent<PlayerController>());
+            }
+        }
+
+        foreach (PlayerController player in players)
+        {
+            cameraTargetGroup.AddMember(player.transform, 1f, 1f);
+        }
+        
         StartPatrol();
     }
 
