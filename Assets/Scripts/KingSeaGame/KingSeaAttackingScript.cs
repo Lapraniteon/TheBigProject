2234f4d36f97c845f7c3d876e94aaebdc35a106e
Zhfwade <90205145+Zhfwade@users.noreply.github.com>
2026-06-24 15:51:02 +0200

    King Sea Attack Animation

--- Assets/Scripts/KingSeaGame/KingSeaAttackingScript.cs
+++ Assets/Scripts/KingSeaGame/KingSeaAttackingScript.cs
@@ -14,15 +14,31 @@ public class KingSeaAttackingScript : MonoBehaviour
         Attacking();
     }
     
+    //Move tentacle in position
     private void Attacking()
     {
-        transform.DOLocalRotate(new Vector3(90, 0, 0),kingSeaAttackDuration);
-        Invoke("PlatformsSplitting", 0.2f);
+        transform.DOMoveY(transform.position.y + 8, kingSeaAttackDuration).SetEase(Ease.OutFlash);
+        Invoke("PlatformsSplitting", 4f);
     }
 
+    //Split the platform
     private void PlatformsSplitting()
     {
         platform1.transform.DOMoveX(platform1.transform.position.x - splitDistance / 2, platformSplittingDuration).SetEase(Ease.OutFlash);
         platform2.transform.DOMoveX(platform2.transform.position.x + splitDistance / 2, platformSplittingDuration).SetEase(Ease.OutFlash);
+        Retreating();
+    }
+
+    //move the tentacle down again
+    private void Retreating()
+    {
+        transform.DOMoveY(transform.position.y - 15, kingSeaAttackDuration).SetEase(Ease.OutFlash);
+        Invoke("DisableArm", 2f);
+    }
+
+    //disable the arm again.
+    private void DisableArm()
+    {
+        transform.gameObject.SetActive(false);
     }
 }
