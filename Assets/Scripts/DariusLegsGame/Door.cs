ac6f32c9b1e76196eb4afe2cac7c928d4c4c8b02
Lapraniteon <lapraniteon@gmail.com>
2026-06-26 15:16:42 +0200

    Darius door collider fix

--- Assets/Scripts/DariusLegsGame/Door.cs
+++ Assets/Scripts/DariusLegsGame/Door.cs
@@ -6,9 +6,18 @@ public class Door : MonoBehaviour
     [SerializeField] private Transform doorLeft;
     [SerializeField] private Transform doorRight;
 
+    private Collider _doorCollider;
+
+    private void Start()
+    {
+        _doorCollider = GetComponent<Collider>();
+        _doorCollider.enabled = false;
+    }
+    
     public void CloseDoor()
     {
         doorLeft.DOLocalRotate(Vector3.zero, .5f).SetEase(Ease.InOutBack);
         doorRight.DOLocalRotate(Vector3.zero, .5f).SetEase(Ease.InOutBack);
+        _doorCollider.enabled = true;
     }
 }
